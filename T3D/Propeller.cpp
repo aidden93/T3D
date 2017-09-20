454b497a42ff343ee285a82640a908993be4954c
aidden <aidde@DESKTOP-31NNOHU>
2017-09-20 23:24:34 +1000

    Created the propeller class and header

---
+++ T3D/Propeller.cpp
@@ -0,0 +1,12 @@
+#include "Propeller.h"
+
+namespace T3D {
+
+	Propeller::Propeller(float radius, float hubRadius, float height, float attackAngle, int blades, int density) {
+		initArrays()
+	}
+
+	Propeller::~Propeller(void) {
+
+	}
+}
